2ee16925cbbf161a18b999af9f28a61a39a4f368
PedroHenriquevr <pedrohvribeiro@gmail.com>
2026-06-08 20:56:07 -0300

    remove web and add mobile app

--- frontend/src/services/auth.service.ts
+++
@@ -1,9 +0,0 @@
-import { api } from "./api.service";
-
-export const authService = {
-  login: async (credentials: any) => {
-    // Note: This endpoint is /auth/login based on backend
-    const response = await api.post("/../auth/login", credentials);
-    return response.data;
-  }
-};
